9af37243116e3cb283035e0911f0d8256a0f01c3
svtzoneph <accstream92@gmail.com>
2026-05-15 20:25:19 +0800

    Update request.js

--- js/request.js
+++ js/request.js
@@ -215,7 +215,7 @@ onAuthStateChanged(auth, async (user) => {
         currentUserData = snapshot.val();
         if (currentUserData.status !== "approved") {
           await signOut(auth);
-          window.location.href = "index.html";
+          window.location.href = "index";
           return;
         }
 
@@ -235,6 +235,6 @@ onAuthStateChanged(auth, async (user) => {
       }
     } catch (error) { console.error("Auth error:", error); }
   } else {
-    window.location.href = "index.html";
+    window.location.href = "index";
   }
 });
