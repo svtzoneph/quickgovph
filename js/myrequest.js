a09664dcd812f8bedba469f8fba8afaf870ddcd4
svtzoneph <accstream92@gmail.com>
2026-05-15 20:26:28 +0800

    Update myrequest.js

--- js/myrequest.js
+++ js/myrequest.js
@@ -179,7 +179,7 @@ onAuthStateChanged(auth, async (user) => {
         currentUserData = snapshot.val();
         if (currentUserData.status !== "approved") {
           await signOut(auth);
-          window.location.href = "index.html";
+          window.location.href = "index";
           return;
         }
 
